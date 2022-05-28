7836d0c5f35d90c394cefbbefe6352f880c373f3
coursek <course.k.ky@gmail.com>
2022-05-28 14:33:11 +0900

    test commit

---
+++ pypack/main.py
@@ -0,0 +1,26 @@
+'''
+Created on 2022/05/21
+
+@author: Kousuke
+'''
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def render_form():
+	return render_template('login.html')
+
+
+@app.route('/', methods=['POST'])
+def login():
+	if request.form['username'] and request.form['email']:
+		return render_template('check.html', email=request.form['email'], username=request.form['username'])
+	else:
+		return render_template('error.html')
+
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
