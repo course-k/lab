acd06978bcb5070c8eb604c61396ef756609ea2e
coursek <course.k.ky@gmail.com>
2022-06-18 10:54:15 +0900

    テンプレート作成

--- pypack/main.py
+++ pypack/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def render_form():
-	return render_template('login.html')
+	return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
